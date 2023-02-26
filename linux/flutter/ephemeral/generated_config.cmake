bb4c7e716d678253de3076e66494c2d36a9e6d10
YedDev <85662790+Saidhfz11@users.noreply.github.com>
2023-02-26 16:22:36 +0700

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/yeddev/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/yeddev/Downloads/ngoding_kampus/flutter/flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/yeddev/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/yeddev/Downloads/ngoding_kampus/flutter/flutter_application_1"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/yeddev/Downloads/ngoding_kampus/flutter/flutter_application_1/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/yeddev/Downloads/ngoding_kampus/flutter/flutter_application_1/lib/main.dart"
+)
